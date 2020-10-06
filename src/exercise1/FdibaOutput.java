0d11f330924e6d552de034f494419e635fdc5551
MartoRam <mmmladenov1@gmail.com>
2020-10-06 15:32:45 +0300

    a

--- src/exercise1/FdibaOutput.java
+++ src/exercise1/FdibaOutput.java
@@ -7,7 +7,7 @@ public class FdibaOutput {
         System.out.println("F     D    D  II B    B    A A");
         System.out.println("FFFF  D     D II BBBBB    AAAAA");
         System.out.println("F     D    D  II B    B  A     A");
-        System.out.println("F     DDDDD   II BBBBB  A       A");
+        System.out.println ("F     DDDDD   II BBBBB  A       A");
     }
 
 }
